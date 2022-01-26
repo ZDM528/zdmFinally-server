bc1a45c5d03a20797484752d9dc41f9e2d594633
张丹敏 <1350349775@qq.com>
2022-01-26 20:39:32 +0800

    增加修改个人信息修改

--- frontend/login.js
+++ frontend/login.js
@@ -38,4 +38,29 @@ router.post('/getScore', (req, res) => {
     })
 })
 
+router.post('/editPersonInfo', (req, res) => {
+    const { username, password, id } = req.body;
+    let sql = "select * from frontendUser";
+    connection.query(sql, (err, data) => {
+        const resData = JSON.parse(JSON.stringify(data));
+        let result = resData.some(item => {
+            if (item.username == username) {
+                res.send({ code: 400, messgae: '该用户已经存在' });
+                return true;
+            }
+        })
+        if (!result) {
+            let sql = `update frontendUser set username='${username}' ,password='${password}' where id=${id}`;
+            connection.query(sql, (err, data) => {
+                if (!err) {
+                    res.send({ code: 200, message: '更新个人信息成功' })
+                } else {
+                    res.send({ code: 403, message: '更新个人信息失败' })
+                }
+            })
+        }
+    })
+})
+
+
 export default router
